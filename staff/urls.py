0a002fc0974b1ef67b5cdd54fa85a7e4a42d9af9
Vishal07003 <vbharti1907@gmail.com>
2026-09-26 16:48:44 +0530

    feat: add staff teacher detail view and supporting backend modules

--- staff/urls.py
+++ staff/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from .views import StaffDashboardHomeView, StaffLoginView, StaffLogoutView
+from .views import (
+    StaffDashboardHomeView,
+    StaffLoginView,
+    StaffLogoutView,
+    TeacherCreateView,
+    TeacherDetailView,
+    TeacherEditView,
+    TeacherListView,
+    TeacherToggleStatusView,
+)
 
 app_name = "staff"
 
@@ -7,4 +16,9 @@ urlpatterns = [
     path("", StaffDashboardHomeView.as_view(), name="dashboard"),
     path("login/", StaffLoginView.as_view(), name="login"),
     path("logout/", StaffLogoutView.as_view(), name="logout"),
+    path("teachers/", TeacherListView.as_view(), name="teachers"),
+    path("teachers/create/", TeacherCreateView.as_view(), name="teacher_create"),
+    path("teachers/<uuid:id>/", TeacherDetailView.as_view(), name="teacher_detail"),
+    path("teachers/<uuid:id>/edit/", TeacherEditView.as_view(), name="teacher_edit"),
+    path("teachers/<uuid:id>/toggle-status/", TeacherToggleStatusView.as_view(), name="teacher_toggle_status"),
 ]
